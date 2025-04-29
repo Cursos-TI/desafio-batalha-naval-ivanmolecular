22e4501d159f6e2fcaf862ce33112bdc3b7ff18a
Ivan Martin Zborowski <161751855+ivanmolecular@users.noreply.github.com>
2025-04-29 00:12:50 +0000

    Desafio aventureiro de vetor e matriz: Batalha naval com mais navios.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,7 +4,8 @@ int main() {
     int tabuleiro[10][10];
     int i, j;
 
-    printf("##Tabuleiro de batalha naval##"); //Cabecalho / Titulo do jogo
+    printf("## Tabuleiro de Batalha Naval ##\n");
+
     // Inicializa o tabuleiro com agua (0)
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
@@ -12,40 +13,38 @@ int main() {
         }
     }
 
-    // Coordenadas iniciais dos navios
-    int linha_horizontal = 2, coluna_horizontal = 4;
-    int linha_vertical = 5, coluna_vertical = 7;
+    // --- Posicionamento dos navios conforme pedido ---
 
-    // Validacao para garantir que os navios estejam dentro do tabuleiro
-    if (coluna_horizontal + 2 >= 10 || linha_vertical + 2 >= 10) {
-        printf("Erro: Coordenadas invalidas para os navios!\n");
-        return 1;
-    }
+    // Navio diagonal \ de A1 ate C3
+    tabuleiro[0][0] = 3; // A1
+    tabuleiro[1][1] = 3; // B2
+    tabuleiro[2][2] = 3; // C3
 
-    // Posiciona o navio horizontal
-    for (i = 0; i < 3; i++) {
-        tabuleiro[linha_horizontal][coluna_horizontal + i] = 3;
-    }
+    // Navio horizontal de F3 ate H3
+    tabuleiro[2][5] = 3; // F3
+    tabuleiro[2][6] = 3; // G3
+    tabuleiro[2][7] = 3; // H3
 
-    // Posiciona o navio vertical (verificando se nao ha sobreposicao)
-    for (i = 0; i < 3; i++) {
-        if (tabuleiro[linha_vertical + i][coluna_vertical] == 3) {
-            printf("Erro: Sobreposicao de navios!\n");
-            return 1;
-        }
-        tabuleiro[linha_vertical + i][coluna_vertical] = 3;
-    }
+    // Navio vertical de D5 ate D7
+    tabuleiro[4][3] = 3; // D5
+    tabuleiro[5][3] = 3; // D6
+    tabuleiro[6][3] = 3; // D7
+
+    // Navio diagonal / de H7 ate J9
+    tabuleiro[6][7] = 3; // H7
+    tabuleiro[7][8] = 3; // I8
+    tabuleiro[8][9] = 3; // J9
 
     // Exibe o cabecalho das colunas (letras de A ate J)
-    printf("\n  "); // Espaco para alinhar os numeros das linhas
+    printf("\n   ");
     for (j = 0; j < 10; j++) {
         printf(" %c", 'A' + j);
     }
     printf("\n");
 
-    // Exibe o tabuleiro com numeracao das linhas
+    // Exibe o tabuleiro com numeração das linhas
     for (i = 0; i < 10; i++) {
-        printf("%2d", i + 1); // Numeros de 1 ate 10 alinhados
+        printf("%2d ", i + 1);
         for (j = 0; j < 10; j++) {
             printf(" %d", tabuleiro[i][j]);
         }
